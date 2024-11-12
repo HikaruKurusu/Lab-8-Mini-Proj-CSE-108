bdd917127df39ac5327be8139747be6cc6f2ef60
Kyle Sulit <ksulit77@gmail.com>
2024-11-12 13:11:24 -0800

    created heading for StudentHome

--- lab08/src/student_pages/StudentHome.jsx
+++ lab08/src/student_pages/StudentHome.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import './StudentHome.css';
-
+import React from "react";
+import "./StudentHome.css";
+import { useNavigate } from "react-router-dom";
 
 function StudentHome() {
-    return (
-        <div className= "student-home" >
-        <h1>Welcome to the Student Dashboard</h1>
-        </div>
+  const navigate = useNavigate();
+  const studentName = localStorage.getItem("studentName") || "Student";
 
-    );
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
+  return (
+    <div className="student-home">
+      <header className="header-container">
+        <div className="top-header">
+          <h2 className="welcome-message">Welcome {studentName}!</h2>
+
+          <button className="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+        <h1 className="welcome-message">University Of California Merced</h1>
+      </header>
+    </div>
+  );
 }
 
 export default StudentHome;
