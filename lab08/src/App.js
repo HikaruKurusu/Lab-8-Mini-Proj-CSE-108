7e014f488a22c91baf3dbc4c1d27a3c98310cd31
Kyle Sulit <ksulit77@gmail.com>
2024-11-12 11:42:43 -0800

    created home pages and buttons

--- lab08/src/App.js
+++ lab08/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginRegister from './LoginPage/LoginRegister';
+import StudentHome from './student_pages/StudentHome';
+import TeacherHome from './teacher_pages/TeacherHome';
+import AdminHome from './admin_pages/AdminHome';
 
 function App() {
   return (
+    
+    <Router>
     <div className="App">
-      <LoginRegister />
+      <Routes>
+        <Route path="/" element={<LoginRegister />} />
+        <Route path="/student-home" element={<StudentHome />} />
+        <Route path="/teacher-home" element={<TeacherHome />} />
+        <Route path="/admin-home" element={<AdminHome />} />
+      </Routes>
     </div>
+  </Router>
+
   );
 }
 
--- lab08/src/LoginPage/LoginRegister.jsx
+++ lab08/src/LoginPage/LoginRegister.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './LoginRegister.css';
+import { useNavigate } from "react-router-dom";
+
 
 const LoginRegister = () => {
+
+  const navigate = useNavigate(); // For navigation after login
+
+
   return (
     <div className="login">
       <h1>Login/Register</h1>
@@ -16,7 +22,44 @@ const LoginRegister = () => {
         </div>
         <button className="signInButton" type="submit">Sign In</button>
       </form>
+
+      <div
+          className="button-container"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "10px",
+            marginTop: "20px",
+          }}
+        >
+          <button
+            variant="contained"
+            className="circle-button"
+            onClick={() => navigate("/student-home")}
+            style={{ borderRadius: "50%", padding: "20px 30px" }}
+          >
+            Student
+          </button>
+          <button
+            variant="contained"
+            className="circle-button"
+            onClick={() => navigate("/teacher-home")}
+            style={{ borderRadius: "50%", padding: "20px 30px" }}
+          >
+            Teacher
+          </button>
+          <button
+            variant="contained"
+            className="circle-button"
+            onClick={() => navigate("/admin-home")}
+            style={{ borderRadius: "50%", padding: "20px 30px" }}
+          >
+            Admin
+          </button>
+      </div>
+      
     </div>
+    
   );
 }
 
