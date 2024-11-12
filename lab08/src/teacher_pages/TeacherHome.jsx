ddfe274a1951d0e508fb391c559a95e45c4a9110
Ethan Villarosa <eacvillarosa@gmail.com>
2024-11-12 11:59:10 -0800

    initial Teacher home page

--- lab08/src/teacher_pages/TeacherHome.jsx
+++ lab08/src/teacher_pages/TeacherHome.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './TeacherHome.css';
 
+const TeacherHome = () => {
+    const [name, setName] = React.useState('Teacher Home');
 
-function TeacherHome() {
-    return (
-        <div className= "teacher-home" >
-        <h1>Welcome to the Teacher Dashboard</h1>
+    return(
+        <div className="outerTeacherHome">
+            <div className="outerHeader">
+                <span className="left">Welcome {name}</span>
+                <span className="center">ACME University</span>
+                <span className="right">Sign out</span>
+            </div>
+            <div className="innerTeacherHome">
+                <h2 className="innerHeader">Your Courses</h2>
+                <table className="courses">
+                    <thead>
+                        <tr>
+                            <th>Course Name</th>
+                            <th>Teacher</th>
+                            <th>Time</th>
+                            <th>Students Enrolled</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
+    )
 
-    );
 }
 
-export default TeacherHome;
+export default TeacherHome
