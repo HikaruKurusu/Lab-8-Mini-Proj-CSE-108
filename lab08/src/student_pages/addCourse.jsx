e652cfd4506ca1e280f32b20ef5c7144df1913b4
Hikaru <epichikaru@gmail.com>
2024-11-16 09:14:16 -0800

    Finished UI for student

--- lab08/src/student_pages/addCourse.jsx
+++ lab08/src/student_pages/addCourse.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './addCourse.css';
 
-function addCourse() {
+function AddCourse() {
+  const [name, setName] = useState('Student');
+  const navigate = useNavigate();
+
+  const handleButtonClick = (buttonName) => {
+    if (buttonName === 'addCourses') {
+      alert('Already in add courses');
+    } else if (buttonName === 'yourCourses') {
+      navigate('/student-home');
+    } else if (buttonName === 'signOut') {
+      alert('Signing out...');
+    }
+  };
+
   return (
-    <div>addCourse</div>
-  )
+    <div className="outeraddCourseHome">
+      <div className="outerHeader">
+        <span className="left">Welcome {name}</span>
+        <span className="center">ACME University</span>
+        <button className="right" onClick={() => handleButtonClick('signOut')}>
+          Sign out
+        </button>
+      </div>
+      <div className="Header">
+        <button
+          className="left"
+          onClick={() => handleButtonClick('yourCourses')}
+        >
+          Your Courses
+        </button>
+        <button
+          className="rights"
+          onClick={() => handleButtonClick('addCourses')}
+        >
+          Add Courses
+        </button>
+      </div>
+      <div className="innerTeacherHome">
+        <h2 className="innerHeader">Add Courses</h2>
+        <table className="courses">
+          <thead>
+            <tr>
+              <th>Course Name</th>
+              <th>Teacher</th>
+              <th>Time</th>
+              <th>Students Enrolled</th>
+              <th>Add Class</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default addCourse
+export default AddCourse;
