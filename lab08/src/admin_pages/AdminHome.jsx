ec7b5968bd2a2ef6562a7dbbb3be8bd6082f1f28
Kyle Sulit <ksulit77@gmail.com>
2024-11-16 10:56:58 -0800

    AdminHome

--- lab08/src/admin_pages/AdminHome.jsx
+++ lab08/src/admin_pages/AdminHome.jsx
@@ -1,65 +1,53 @@
-import React, { useState } from 'react';
-import './AdminHome.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./AdminHome.css";
+import { useNavigate } from "react-router-dom";
 
 function AdminHome() {
+  const [name, setName] = useState("Admin");
+  const navigate = useNavigate();
 
-    const [name, setName] = useState('Admin');
-    const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
 
-    const handleLogout = () => {
-        localStorage.clear();
-        navigate('/');
-    };
-    
-    const handleButtonClick = (buttonName) => {
-        if (buttonName === 'addCourses') {
-            navigate('/add-course');
-        } else if (buttonName === 'yourCourses') {
-            navigate('/student-home');
-        }
-    };
+  const handleButtonClick = (buttonName) => {
+    if (buttonName === "admin-courses") {
+      navigate("/add-course");
+    } else if (buttonName === "admin-instructor-teaches") {
+      navigate("/student-home");
+    } else if (buttonName === "admin-student-enrolledin") {
+      navigate("/student-home");
+    } else if (buttonName === "admin-user-info") {
+      navigate("/student-home");
+    }
+  };
 
-
-    return (
-        <div className="outerStudentHome">
-        <div className="outerHeader">
-            <span className="left">Welcome {name}</span>
-            <span className="center">ACME University</span>
-            <button className="right" onClick={handleLogout}>
-                Sign out
-            </button>
-        </div>
-        <div className="Header">
-            <button
-                className="lefts"
-                onClick={() => handleButtonClick('yourCourses')}
-            >
-                Your Courses
-            </button>
-            <button
-                className="right"
-                onClick={() => handleButtonClick('addCourses')}
-            >
-                Add Courses
-            </button>
-        </div>
-        <div className="innerTeacherHome">
-            <h2 className="innerHeader">Your Courses</h2>
-            <table className="courses">
-                <thead>
-                    <tr>
-                        <th>Course Name</th>
-                        <th>Teacher</th>
-                        <th>Time</th>
-                        <th>Students Enrolled</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
+  return (
+    <div className="outerAdminHome">
+      <div className="outerHeader">
+        <span className="left">Welcome {name}</span>
+        <span className="center">ACME University</span>
+        <button className="right" onClick={handleLogout}>
+          Sign out
+        </button>
+      </div>
+      <div className="Header">
+        <button
+          className="navButton"
+          onClick={() => handleButtonClick("yourCourses")}
+        >
+          Your Courses
+        </button>
+        <button
+          className="navButton"
+          onClick={() => handleButtonClick("addCourses")}
+        >
+          Add Courses
+        </button>
+      </div>
     </div>
-
-    );
+  );
 }
 
 export default AdminHome;
