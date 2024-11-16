e467ea13ac326473b065a3ac53e2a3e5dcf46ef3
ricecakes119 <jasonelcano119@gmail.com>
2024-11-16 10:22:17 -0800

    dbupdate3

--- lab08/src/LoginPage/addUser.py
+++ lab08/src/LoginPage/addUser.py
@@ -1,7 +1,8 @@
 from app import db, UserInfo, app  # Import the app, db, and Student model from your main app file
 
 # Create a new Student instance
-new_user = UserInfo(name="Hikaru", password="123", userType="student")
+new_user = UserInfo(name="Jason", password="123", userType="admin")
+
 
 # Use app context to interact with the database
 with app.app_context():
